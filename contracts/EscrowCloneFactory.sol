b82cf9b7d405624eaebe0b014516f6024c5e35ac
Bradley Woolf <bradleymwoolf@gmail.com>
2022-02-06 19:16:35 -0800

    remove merge cinflicts

--- contracts/EscrowCloneFactory.sol
+++ contracts/EscrowCloneFactory.sol
@@ -16,30 +16,19 @@ contract EscrowCloneFactory {
         implementationContract = _implementation;
     }
 
-<<<<<<< HEAD:contracts/Freeflow/EscrowCloneFactory.sol
-    function createNewEscrow(address freeflow, address _client, address payable _payee, uint256 _initialAmount) payable external returns (address instance) {
-=======
+    /// @dev Client deploys the escrow contract
     function createNewEscrow(address _client, address payable _dev, address payable _freeflow, bool isEth) payable external returns (address instance) {
->>>>>>> 40ee5704b2ffd4224e31ff8b8e2fb50bfe4579a9:contracts/EscrowCloneFactory.sol
         instance = Clones.clone(implementationContract);
         
         (bool success, ) = instance.call{
             value: msg.value
             }(
                 abi.encodeWithSignature(
-<<<<<<< HEAD:contracts/Freeflow/EscrowCloneFactory.sol
-                    "initialize(address,address,address)", 
-                    freeflow,
-                    _client,
-                    _payee,
-                    _initialAmount
-=======
                     "initialize(address,address,address,bool)", 
                     _client,
                     _dev,
                     _freeflow,
                     isEth
->>>>>>> 40ee5704b2ffd4224e31ff8b8e2fb50bfe4579a9:contracts/EscrowCloneFactory.sol
                 ));
 
         allClones.push(instance);
